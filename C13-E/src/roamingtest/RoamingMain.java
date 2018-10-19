fa9911c60e8c10806b116f602e51200a6b503bfb
BR <bromeijn@hotmail.com>
2018-10-19 16:17:49 +0200

    Roaming aangepast

--- C13-E/src/roamingtest/RoamingMain.java
+++ C13-E/src/roamingtest/RoamingMain.java
@@ -1,53 +1,9 @@
 package roamingtest;
 
-/*private int motorSpeedA;
-private int motorSpeedB;
-GraphicsLCD LCD = BrickFinder.getDefault().getGraphicsLCD();
-static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
-
-
-public void moverobotleft() {
-	this.motorSpeedA = 0;
-	this.motorSpeedB = 400;
-	leftMotor.setSpeed(motorSpeedA);
-	rightMotor.setSpeed(motorSpeedB);
-	leftMotor.forward();
-	rightMotor.backward();
-}
-
-public void moverobotright() {
-	this.motorSpeedA = 400;
-	this.motorSpeedB = 0;
-	leftMotor.setSpeed(motorSpeedA);
-	rightMotor.setSpeed(motorSpeedB);
-	leftMotor.forward();
-	rightMotor.backward();
-
-}
-
-public void moverobotStraightForward() {
-	this.motorSpeedA = 400;
-	this.motorSpeedB = 400;
-	leftMotor.setSpeed(motorSpeedA);
-	rightMotor.setSpeed(motorSpeedB);
-	leftMotor.forward();
-	rightMotor.forward();
-}
-
-public void moverobotStraightBackward() {
-	this.motorSpeedA = 400;
-	this.motorSpeedB = 400;
-	leftMotor.setSpeed(motorSpeedA);
-	rightMotor.setSpeed(motorSpeedB);
-	leftMotor.backward();
-	rightMotor.backward();
-}
-}*/
-
 import java.io.File;
 
 import customrobot.library.TouchSensor;
+import lejos.ev3.tools.LCDDisplay;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
@@ -63,6 +19,7 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.lcd.TextLCD;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
@@ -75,18 +32,34 @@ public class RoamingMain {
 	static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
 	static TouchSensor touch = new TouchSensor(SensorPort.S3);
 	static RegulatedMotor motorC = new EV3MediumRegulatedMotor(MotorPort.C);
+	static GraphicsLCD LCD = BrickFinder.getDefault().getGraphicsLCD();
+	private static final int NUMBER_OF_ACTIONS = 4;
+	private static final int START_NUMBER = 1;
+	private static final int MAX_DEGREES = 60;
+	private static final int START_DEGREES = 30;
+	private static final int START_KWISPELS = 3;
+	private static final int MAX_KWISPELS = 8;
+	private static final int STARTPOINT_TAIL = 0;
+	private static final int ACTION_1 = 1;
+	private static final int ACTION_2 = 2;
+	private static final int ACTION_3 = 3;
+	private static final int AVOID_SPEED_HIGH = 120;
+	private static final int AVOID_SPEED_LOW = 40;
 
 	public static void main(String[] args) {
 
 		Sound.beepSequenceUp(); // make sound when ready.
-
-		System.out.println("Druk om een knop om fikkie te laten zwerven");
-
+		LCD.drawString("PUSH MY BUTTON", 100, 20, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
 		Button.waitForAnyPress();
+		// TODO: Draw doggy eyes
+		LCD.clear();
+		Delay.msDelay(1000);
+		LCD.drawString("ROAMING MODE", 100, 20, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
 
 		Sound.playSample(new File("dog_bark6.wav"), Sound.VOL_MAX);
-		motorA.setPower(100);
-		motorB.setPower(100);
+
+		motorA.setPower(200);
+		motorB.setPower(200);
 
 		// poort initialiseren
 		Port port = LocalEV3.get().getPort("S2");
@@ -119,20 +92,17 @@ public class RoamingMain {
 			int dist = (int) sample[0];
 
 			while (dist < 35 && Button.ESCAPE.isUp()) {
-				// TODO maak een switch case om een modus te kiezen wanneer er iets in de sensor
+				// TODO maak een switch case om een modus te kiezen wanneer er iets voor de
+				// sensor
 				// gedetecteerd wordt.
 
-				switch (makeRandomActie()) {
-				case 1: //  ga naar links
-					motorA.forward();
-					motorA.setPower(90);
-					motorB.setPower(30);
+				switch (makeRandomNumber(ACTION_1)) {
+				case 1: // ga naar links
+					avoidleft();
 					break;
 				// zet 'm naar rechts
 				case 2:
-					motorB.forward();
-					motorB.setPower(90);
-					motorA.setPower(30);
+					avoidRight();
 					break;
 				case 3:
 					// blaf
@@ -159,46 +129,55 @@ public class RoamingMain {
 			}
 		}
 
-		// Stop motors with brakes on.
+		// stop de motoren
 		motorA.stop();
 		motorB.stop();
+		motorC.stop();
 
-		// Free up resources.
+		// sluit de motoren
 		motorA.close();
 		motorB.close();
+		motorC.close();
 
-		Sound.playSample(new File("dog_bark6.wav"), Sound.VOL_MAX);// Finish programme
+		Sound.playSample(new File("dog_bark6.wav"), Sound.VOL_MAX);
+		// Blaf nog één keer als klaar.
 	}
 
-	public static int makeRandomActie() {
-		int random = (int) (Math.random() * 4) + 1;
+	public static int makeRandomNumber(int actie) {
+		int random = 0;
+		if (actie == ACTION_1) {
+			random = (int) (Math.random() * NUMBER_OF_ACTIONS) + START_NUMBER;
+		} else if (actie == ACTION_2) {
+			random = (int) (Math.random() * MAX_DEGREES) + START_DEGREES;
+		} else if (actie == ACTION_3) {
+			random = (int) (Math.random() * MAX_KWISPELS) + START_KWISPELS;
+		}
 		return random;
 	}
-	
-	public static int makeRandomDegrees() {
-		int random = (int) (Math.random() * 60) + 30;
-		return random;
-	}
-	
-	public static int makeRandomKwispelen() {
-		int random = (int) (Math.random() * 15) + 5;
-		return random;
-	}
-	
-	
 
 	public static void wagTail() {
-		// TODO laat een aantal keer kwispelen
-		for (int aantalKeer = 0; aantalKeer < makeRandomKwispelen(); aantalKeer++) {
-		motorC.setSpeed(600);
-		//motorC.forward();
-		motorC.rotateTo(makeRandomDegrees());
-		Delay.msDelay(1);
-		//motorC.backward();
-		motorC.rotateTo(-makeRandomDegrees());
+
+		for (int aantalKeer = 0; aantalKeer < makeRandomNumber(3); aantalKeer++) {
+			motorC.setSpeed(600);
+			motorC.rotateTo(makeRandomNumber(ACTION_2));
+			Delay.msDelay(1);
+			motorC.rotateTo(-makeRandomNumber(ACTION_2));
 		}
-		motorC.rotateTo(0);
-		//make random 
+		motorC.rotateTo(STARTPOINT_TAIL);
+		// make random
+	}
+
+	public static void avoidleft() {
+
+		motorB.forward();
+		motorB.setPower(AVOID_SPEED_HIGH);
+		motorA.setPower(AVOID_SPEED_LOW);
+	}
+
+	public static void avoidRight() {
+		motorA.forward();
+		motorA.setPower(AVOID_SPEED_HIGH);
+		motorB.setPower(AVOID_SPEED_LOW);
 	}
 
 }
